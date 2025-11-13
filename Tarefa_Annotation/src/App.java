73c1c97a483e780047bb6d0d77dcfa77b2af2ac4
Leonardo <leonardo@aulas.com>
2025-11-13 12:09:12 -0300

    Atividade_Reflexao

--- Tarefa_Annotation/src/App.java
+++ Tarefa_Annotation/src/App.java
@@ -1,5 +1,37 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Class Tclass = Tabela.class;
+        System.out.println("Anotação: " + Tclass);
+        
+        
+// PODE IGNORAR O CÓDIGO ABAIXO, estou apenas revisando a aula de reflexão
+
+        // Class clazz = Teste.class;
+
+        // System.out.println("Esta classe é: " + clazz);
+
+        // Teste teste1 = new Teste();
+        // Class classeTeste1 = teste1.getClass();
+
+        // System.out.println("A classe do objeto teste1 é: " + classeTeste1.getName());
+
+
+        // try {
+        //     Constructor cons = clazz.getConstructor();
+        //     Teste teste2 = (Teste) cons.newInstance();
+        //     System.out.println("Objeto criado via reflexão: " + teste2);
+
+        //     Field[] fields = teste2.getClass().getDeclaredFields();
+
+        //     for(Field field : fields){
+        //         System.out.println("Campo: " + field.getName() + " | Tipo: " + field.getType());
+        //     }
+
+        // }catch(NoSuchMethodException e){
+        //     System.out.println("O construtor não foi encontrado.");
+        // }
     }
 }
---
+++ Tarefa_Annotation/src/Teste.java
@@ -0,0 +1,10 @@
+// public class Teste {
+//     public String nome;
+
+//     public Long id;
+
+//     public Double valor;
+
+//     public Teste() {
+//     }
+// }
